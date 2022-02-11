6cee38a998c26289f6354f4add2969c739c8536b
Vicki D Bealman <vbealman@fullsail.com>
2022-02-11 15:57:06 -0500

    Initial commit

---
+++ src/Counter.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="counter">
+      <h2>Counter</h2>
+      <div>
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
